1541efb6e23bb8e3ba0c68baed13ea555ab54f69
Pavel Egorov <snake7799@users.noreply.github.com>
2017-02-28 13:45:17 +0300

    minimal-number-system/feature: implement getBase method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-module.exports = function getBase(base) {
-  // your solution
+module.exports = function getBase(number) {
+	let copyNum = number;
+	let base = 2;
+	
+	if (copyNum[0] == '1' && copyNum.split('').slice(1).every((currentValue) => currentValue == '0'))
+		return '9'.repeat(copyNum.length - 1);
+	
+	while (copyNum) {
+		if (copyNum % base == 1)
+			copyNum = Math.floor(copyNum / base);
+		else {
+			copyNum = number;
+			base++;
+		}
+	}
+	return base;
 }
